3c59656cd99109b04d71d2a5cc37423bd89573eb
David Ivan <andreidavid96@yahoo.com>
2016-04-28 21:57:14 +0300

    Adaugat inserare, echilibrare
    + schimbat build pe MINGW

--- src/Arbori-AVL.cpp
+++ src/Arbori-AVL.cpp
@@ -7,6 +7,7 @@
 //============================================================================
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 struct nod
@@ -15,6 +16,8 @@ struct nod
 	int bal;
 	nod *left, *right;
 };
+
+nod *r;
 
 void rotatie_dreapta(nod *&p)
 {
@@ -48,7 +51,58 @@ void rotatie_stanga_dreapta (nod *&p)
 	rotatie_dreapta(p);
 }
 
+void echilibrare (nod *&p)
+{
+	if (p->bal == -2)
+	{
+		if (p->left->bal == 1)
+			rotatie_stanga_dreapta(p);
+		else rotatie_dreapta(p);
+	}
+	else
+	{
+		if (p->right->bal == -1)
+			rotatie_dreapta_stanga(p);
+		else
+			rotatie_stanga(p);
+	}
+}
+
+bool inserare (nod *&p, int val)
+{
+	if (p == NULL)
+	{
+		p = new nod; // de facut cu smart pointers
+		p->info = val;
+		p->bal = 0;
+		p->left = NULL;
+		p->right = NULL;
+		return true;
+	}
+
+	if (p->info == val) return (false);
+
+	if (p->info > val)
+		if (inserare (p->left, val) == true)
+			p->bal--;
+		else return false;
+	else
+		if (inserare (p->right, val) == true)
+			p->bal++;
+		else return false;
+
+	if ( p->bal == 0 || abs(p->bal) == 1)
+		return true;
+		else
+		{
+			echilibrare(p);
+			return false;
+		}
+}
 int main() {
+
+	inserare (r, 5);
+	inserare (r, 4);
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
