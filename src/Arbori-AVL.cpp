0eae8aaac868d6a3922963fd293da7298fa4a902
David Ivan <andreidavid96@yahoo.com>
2016-04-28 21:31:47 +0300

    Adaugat rotatiile

--- src/Arbori-AVL.cpp
+++ src/Arbori-AVL.cpp
@@ -9,6 +9,45 @@
 #include <iostream>
 using namespace std;
 
+struct nod
+{
+	int info;
+	int bal;
+	nod *left, *right;
+};
+
+void rotatie_dreapta(nod *&p)
+{
+	nod *t = p->left;
+	p->left = t->right;
+	t->right = p;
+	p->bal = p->bal + (1 - min(t->bal, 0));
+	t->bal = t->bal + (1 + max(p->bal, 0));
+	p = t;
+}
+
+void rotatie_stanga(nod *&p)
+{
+	nod *t = p->right;
+	p->right = t->left;
+	t->left = p;
+	p->bal = p->bal - (1+ max(t->bal, 0));
+	t->bal = t->bal - (1-min(p->bal, 0));
+	p = t;
+}
+
+void rotatie_dreapta_stanga (nod *&p)
+{
+	rotatie_dreapta(p->right);
+	rotatie_stanga(p);
+}
+
+void rotatie_stanga_dreapta (nod *&p)
+{
+	rotatie_stanga(p->left);
+	rotatie_dreapta(p);
+}
+
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
