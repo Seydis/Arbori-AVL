79d728075bec1a575467231b199ce9b4a4e0807c
David Ivan <andreidavid96@yahoo.com>
2016-04-29 13:44:22 +0300

    Adaugat stergere + gasit Min si Max

--- src/Arbori-AVL.cpp
+++ src/Arbori-AVL.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <cmath>
+#include <cstdlib>
 using namespace std;
 
 struct nod
@@ -99,10 +100,121 @@ bool inserare (nod *&p, int val)
 			return false;
 		}
 }
+
+nod* maxim (nod *r)
+{
+	if (!r)
+	{
+		cout<<"Arborele nu exista";
+		exit(1);
+	}
+
+	while (r->right)
+		r = r->right;
+	return r;
+}
+
+nod* minim (nod *r)
+{
+	if (!r)
+		{
+			cout<<"Arborele nu exista";
+			exit(1);
+		}
+
+	while (r->left)
+		r = r->left;
+	return r;
+}
+bool stergere (nod *&p, int val)
+{
+	if (p == NULL)
+	{
+		//std::cout<<"Arbore gol, nu se poate sterge";
+		return false;
+	}
+
+	if (p->info > val)
+	{
+		if (stergere(p->left, val) == true)
+			p->bal++;
+		else return false;
+	}
+	else
+		if (p->info < val)
+		{
+			if (stergere (p->right, val) == true)
+				p->bal--;
+			else return false;
+		}
+		else
+		{
+			if (p->left == NULL || p->right == NULL) // are cel mult un copil
+			{
+				if (p->left != NULL)
+				{
+					p->info = p->left->info;
+					p->left = NULL;
+					p->bal++;
+					return true;
+					// poate delete(t->left) aici
+				}
+				else
+					if (p->right != NULL)
+					{
+						p->info = p->right->info;
+						p->right = NULL;
+						p->bal--;
+						return true;
+					}
+			}
+			else // are 2 copii
+			{
+				nod *t = minim(p->right);
+				p->info = t->info;
+				if (stergere (p->right, t->info) == true)
+					p->bal--;
+				else
+					return false;
+			}
+		}
+
+	if (abs(p->bal) != 0)
+		echilibrare(p);
+
+	if (p->bal == 0)
+		return true;
+	else
+		return false;
+
+}
+
+void rsd (nod *rad)
+{
+    if (rad)
+    {
+        std::cout<<rad->info<<" ";
+        rsd(rad->left);
+        rsd(rad->right);
+    }
+}
 int main() {
 
-	inserare (r, 5);
-	inserare (r, 4);
+	inserare (r, 35);
+	inserare (r, 37);
+	inserare (r, 30);
+	inserare (r, 50);
+	inserare (r, 45);
+	inserare (r, 40);
+	inserare (r, 39);
+	inserare (r, 42);
+	inserare (r, 55);
+
+	stergere(r, 45);
+	stergere(r, 35);
+
+	rsd (r);
+
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
